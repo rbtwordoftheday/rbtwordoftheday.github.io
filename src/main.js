034264bd91e46bf47cc20562f4d71e893d89ad2e
Neskoff <neskoff@gmail.com>
2021-09-06 12:19:32 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import router from "./router";
+import "./style.css";
+createApp(App)
+  .use(router)
+  .mount("#app");
